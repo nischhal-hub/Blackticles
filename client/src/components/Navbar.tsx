cfd12acb96899691e6cb398dc06e472d0aa58c05
nischhal-nub <nischalbh51@gmail.com>
2024-05-28 12:18:35 +0545

    UI: navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import logo from '../assets/Blackticles_logo.png'
+import { RxHamburgerMenu } from "react-icons/rx";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+      <nav className='flex justify-between items-center p-2 border-solid border-b-2 mx-2'>
+        <div className='w-56' >
+          <img src={logo} alt="Blackticles Logo" className='object-contain'/>
+        </div>
+        <div className='md:hidden text-2xl'>
+          <RxHamburgerMenu />
+        </div>
+        <div className='hidden md:block transition-all mr-4'>
+          <ul className='flex font-playFair space-x-8 text-lg font-medium'>
+            <li>Blog</li>
+            <li>Search</li>
+            <li>About</li>
+          </ul>
+        </div>
+      </nav>
+    </>
   )
 }
 
