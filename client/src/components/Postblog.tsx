96a430f39a765cffc24311be0ff8d2c5296aa2a7
nischhal-nub <nischalbh51@gmail.com>
2024-05-30 15:23:47 +0545

    UI:filter option in articles

--- client/src/components/Postblog.tsx
+++ client/src/components/Postblog.tsx
@@ -38,6 +38,7 @@ const Postblog = () => {
                         <p className='w-[130%] tracking-widest text-xl'>{Array.from({ length: 20 }).map((_, i) => (<span key={i} className='font-light'> Create <span className='font-bold'> Awesometicles </span></span>))}</p>
                     </div>
                 </div>
+                
                 <div className='w-[95%] md:w-[40%] mx-auto mt-6'>
                 <div className="form w-full mt-6">
                         <form onSubmit={handleSubmit(onSubmit)}>
