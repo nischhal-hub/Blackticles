7bcc38b114738c71e890b5ae52e706f38d42c5ab
nischhal-nub <nischalbh51@gmail.com>
2024-05-31 13:45:13 +0545

    configured api

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -15,15 +15,47 @@ export const fetchSingle = async(id:string)=>{
         const resp = await axios(`${url}/single/${id}`)
         return resp.data
     }catch(error){
-        console.log(`Error Fetching Data: ${error}`)
+        console.log(`Error Fetching single blog data: ${error}`)
         throw error
     }
 }
 
-export const postBlog = async()=>{
+export const postBlog = async(blogData:string)=>{
     try{
-        const resp = await axios(`${url}/new`)
+        const resp = await axios.post(`${url}/new`,blogData,{
+            headers:{
+                'Content-type':'application/json'
+            }
+        })
         return resp.data
+    }catch(error){
+        console.log(`Error posting blog: ${error}`)
+        throw error
+    }
+}
+
+export const editBlog = async(blogData:string,id:string)=>{
+    try{
+        const resp = await axios.put(`${url}/edit/${id}`,blogData,{
+            headers:{
+                'Content-type':'application/json'
+            }
+        })
+        return resp.data
+    }catch(error){
+        console.log(`Error editing Data: ${error}`)
+        throw error
+    }
+}
+
+export const deleteBlog = async(id:string)=>{
+    try{
+        const resp = await axios.delete(`${url}/delete/${id}`,{
+            headers:{
+                'Content-type':'application/json'
+            }
+        })
+        return resp
     }catch(error){
         console.log(`Error Fetching Data: ${error}`)
         throw error
