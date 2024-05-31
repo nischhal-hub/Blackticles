df999f38354e9c8221e0044b70fa7386d7aad8d4
nischhal-nub <nischalbh51@gmail.com>
2024-05-31 12:54:54 +0545

    added link to home on clicking logo

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -31,14 +31,14 @@ const Home = () => {
       {/* Hero blog */}
       <div className='w-full mt-10 md:w-[60%] mx-auto'>
         <div className=' border-b-2 border-solid border-slate-900 border-spacing-7 pb-8'>
-          <div className='w-full' >
+          <div className='w-full aspect-video' >
             <img src={thumbnail} alt="picture" className='w-full' />
           </div>
           <h1 className='font-playFair text-4xl font-bold text-center mx-4 mt-2'>A few words about this blog platform, Ghost, and how this site was made.</h1>
           <p className='font-grot font-light text-lg text-center mx-6 mt-4'>Why Ghost (& Figma) instead of Medium, WordPress or other options?</p>
         </div>
         {/* all articles */}
-        <div className='mt-8 w-full mx-auto'>
+        <div className='mt-8 w-full mx-auto border-2'>
           <h3 className='text-center font-playFair text-3xl font-bold'>All articles</h3>
           <div className='mt-4 w-[90%] mx-auto'>
             <div>
