dd96dc324232d2d1a2b357f4ae640886712e4d27
nischhal-nub <nischalbh51@gmail.com>
2024-06-02 13:26:31 +0545

    UX: added animation on modal

--- client/src/components/Modal.tsx
+++ client/src/components/Modal.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import React, { FC } from 'react'
 import ReactDOM from 'react-dom'
 type TModalProp = {
@@ -5,10 +6,18 @@ type TModalProp = {
     onCloseButtonClick: () => void;
 }
 const Modal: FC<TModalProp> = (prop) => {
-    if (!prop.show)
-        return null;
+    const variants = {
+        open: { height: '100%', opacity: 1 },
+        close: { height: 0, opacity: 0 }
+    }
     return ReactDOM.createPortal(
-        <div className="bg-whiteTransparent w-full h-screen fixed top-0 right-0 flex justify-center items-center z-50">
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={prop.show ? 'open' : 'close'}
+            variants={variants}
+            exit={{ height: 0 }}
+            transition={{ duration: .2 }}
+            className="bg-whiteTransparent w-full h-screen fixed top-0 right-0 flex justify-center items-center z-50">
             <div className="relative w-[80%] sm:w-[40%] px-2 h-44 bg-red-600 border-[1px] border-solid border-black flex flex-col justify-center items-center">
                 <div className="absolute top-1 right-3">
                     <button onClick={prop.onCloseButtonClick} className='text-4xl text-white'>&times;</button>
@@ -21,7 +30,7 @@ const Modal: FC<TModalProp> = (prop) => {
                     <button onClick={prop.onCloseButtonClick} className='px-2 py-1 bg-accent font-grot text-sm'>No</button>
                 </div>
             </div>
-        </div>
+        </motion.div>
         , document.body
     )
 }
