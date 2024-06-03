38c00e83e63e12337c016d529174e6045c1fee39
Bipin Baral <baburaja3421@gmail.com>
2024-06-04 01:39:47 +0545

    updated gitignore

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,63 +1,63 @@
 import axios from "axios";
-const url = '/api/blog/getAll'
-export const fetchAll = async()=>{
-    try{
-        const resp = await axios(`${url}/all`)
-        return resp.data
-    }catch(error){
-        console.log(`Error Fetching Data: ${error}`)
-        throw error
-    }
-}
+const url = "/blog";
+export const fetchAll = async () => {
+  try {
+    const resp = await axios(`${url}/all`);
+    return resp.data;
+  } catch (error) {
+    console.log(`Error Fetching Data: ${error}`);
+    throw error;
+  }
+};
 
-export const fetchSingle = async(id:string)=>{
-    try{
-        const resp = await axios(`${url}/single/${id}`)
-        return resp.data
-    }catch(error){
-        console.log(`Error Fetching single blog data: ${error}`)
-        throw error
-    }
-}
+export const fetchSingle = async (id: string) => {
+  try {
+    const resp = await axios(`${url}/single/${id}`);
+    return resp.data;
+  } catch (error) {
+    console.log(`Error Fetching single blog data: ${error}`);
+    throw error;
+  }
+};
 
-export const postBlog = async(blogData:string)=>{
-    try{
-        const resp = await axios.post(`${url}/new`,blogData,{
-            headers:{
-                'Content-type':'application/json'
-            }
-        })
-        return resp.data
-    }catch(error){
-        console.log(`Error posting blog: ${error}`)
-        throw error
-    }
-}
+export const postBlog = async (blogData: string) => {
+  try {
+    const resp = await axios.post(`${url}/new`, blogData, {
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+    return resp.data;
+  } catch (error) {
+    console.log(`Error posting blog: ${error}`);
+    throw error;
+  }
+};
 
-export const editBlog = async(blogData:string,id:string)=>{
-    try{
-        const resp = await axios.put(`${url}/edit/${id}`,blogData,{
-            headers:{
-                'Content-type':'application/json'
-            }
-        })
-        return resp.data
-    }catch(error){
-        console.log(`Error editing Data: ${error}`)
-        throw error
-    }
-}
+export const editBlog = async (blogData: string, id: string) => {
+  try {
+    const resp = await axios.put(`${url}/edit/${id}`, blogData, {
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+    return resp.data;
+  } catch (error) {
+    console.log(`Error editing Data: ${error}`);
+    throw error;
+  }
+};
 
-export const deleteBlog = async(id:string)=>{
-    try{
-        const resp = await axios.delete(`${url}/delete/${id}`,{
-            headers:{
-                'Content-type':'application/json'
-            }
-        })
-        return resp
-    }catch(error){
-        console.log(`Error Fetching Data: ${error}`)
-        throw error
-    }
-}
+export const deleteBlog = async (id: string) => {
+  try {
+    const resp = await axios.delete(`${url}/delete/${id}`, {
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+    return resp;
+  } catch (error) {
+    console.log(`Error Fetching Data: ${error}`);
+    throw error;
+  }
+};
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { AppProvider } from './hooks/useGlobalContext.tsx'
-
-const queryClient = new QueryClient()
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AppProvider } from "./hooks/useGlobalContext.tsx";
+import axios from "axios";
+axios.defaults.baseURL = "http://localhost:5001/api";
+const queryClient = new QueryClient();
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <AppProvider>
@@ -16,5 +17,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         </BrowserRouter>
       </AppProvider>
     </QueryClientProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
