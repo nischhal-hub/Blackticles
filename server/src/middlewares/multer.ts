7d5db3ff48187bea9265ba9404a21b7f15d9a597
Bipin Baral <baburaja3421@gmail.com>
2024-06-04 01:07:35 +0545

    fix: multer error

--- server/src/middlewares/multer.ts
+++ server/src/middlewares/multer.ts
@@ -1,13 +1,12 @@
 import multer from "multer";
 import { v4 as uuid } from "uuid";
+import path from "path";
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "uploads");
   },
   filename: function (req, file, cb) {
-    const id = uuid();
-    const extName = file.originalname.split(".").pop();
-    cb(null, `${id}.${extName}`);
+    cb(null, Date.now() + path.extname(file.originalname));
   },
 });
 
